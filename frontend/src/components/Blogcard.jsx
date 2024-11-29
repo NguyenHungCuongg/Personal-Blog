dd699175e842d38ecefde3416426d8deb8817e00
HungCuong <23520201@gm.uit.edu.vn>
2024-11-29 22:57:02 +0700

    Sửa lại bookmark checkbox của Blog Page

--- frontend/src/components/Blogcard.jsx
+++ frontend/src/components/Blogcard.jsx
@@ -1,13 +1,14 @@
 import { assets } from "../assets/assets";
+import Checkbox from "@mui/material/Checkbox";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { grey } from "@mui/material/colors";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Blogcard() {
@@ -21,9 +22,15 @@ function Blogcard() {
           </Avatar>
         }
         action={
-          <IconButton aria-label="bookmark" sx={{ color: grey[500] }}>
-            <BookmarkIcon />
-          </IconButton>
+          <Checkbox
+            icon={<BookmarkBorderIcon />}
+            checkedIcon={<BookmarkIcon />}
+            sx={{
+              "&.Mui-checked": {
+                color: "secondary.main",
+              },
+            }}
+          />
         }
         title="Author Name"
         subheader="Date"
