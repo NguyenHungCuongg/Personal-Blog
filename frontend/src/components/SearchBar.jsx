c8b77949b0edbe794823ea392e2efa4982fbc329
HungCuong <23520201@gm.uit.edu.vn>
2024-11-29 20:54:35 +0700

    Sửa lại phần Searchbar cho BlogPage

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,13 +3,14 @@ import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import Input from "@mui/material/Input";
+import OutlinedInput from "@mui/material/OutlinedInput";
 
 function SearchBar() {
   return (
-    <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+    <FormControl fullWidth variant="outlined">
       <InputLabel>Search</InputLabel>
-      <Input
+      <OutlinedInput
+        label="Search"
         endAdornment={
           <InputAdornment position="end">
             <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -9,17 +9,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Blog() {
   const isSmallScreen = useMediaQuery("(max-width:768px)");
   return (
-    <div className="container my-5">
-      <div id="blogHeaderSection">
+    <div className="container mt-5" style={{ paddingBottom: "100px" }}>
+      <div id="blogHeaderSection" className="py-5">
         <HeaderText
-          title="Blog Page"
+          title={
+            <>
+              <span style={{ color: "#8f2eb6" }}>Blog</span> Discovery
+            </>
+          }
           description="Explore an eclectic mix of topics, from art and design to technology and travel. My blogs promise something intriguing for everyone."
           containerClass="d-flex flex-column text-center py-5 my-5"
           containerStyle={isSmallScreen ? {} : { paddingLeft: "200px", paddingRight: "200px" }}
           titleClass="display-1 fw-bold text-body-emphasis lh-1 mb-3"
         />
       </div>
-      <div id="blogSearchSection" className={`d-flex row ${isSmallScreen ? "flex-column gap-2" : ""}`}>
+      <div
+        id="blogSearchSection"
+        className={`d-flex row align-items-center ${isSmallScreen ? "flex-column gap-2 align-items-center" : ""}`}
+      >
         <div className={`${isSmallScreen ? "" : "col-8"}`}>
           <SearchBar />
         </div>
@@ -28,7 +35,9 @@ function Blog() {
         </div>
       </div>
       <DeviderLine />
-      <BlogAlbum />
+      <div className="py-5">
+        <BlogAlbum />
+      </div>
     </div>
   );
 }
