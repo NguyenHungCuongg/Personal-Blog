8553d68b37acc0029a5c0b32c460969bb3f60f01
HungCuong <23520201@gm.uit.edu.vn>
2024-12-03 00:50:26 +0700

    Thêm một chút responsive cho blogheader

--- frontend/src/components/BlogHeader.jsx
+++ frontend/src/components/BlogHeader.jsx
@@ -1,16 +1,20 @@
+import useMediaQuery from "@mui/material/useMediaQuery";
 import TagsList from "./TagsList";
 import ByUser from "../components/ByUser";
 import CreateAt from "./CreateAt";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function BlogHeader() {
+  const isSmallScreen = useMediaQuery("(max-width:768px)");
   return (
     <div>
       <h3 className="fs-1 fw-bold">What is Lorem Ipsum?</h3>
-      <div className="d-flex align-items-center gap-4">
+      <div className={`d-flex align-items-center ${isSmallScreen ? "flex-column gap-2" : " gap-4"}`}>
         <TagsList />
-        <ByUser />
-        <CreateAt />
+        <div className="d-flex align-items-center gap-4">
+          <ByUser />
+          <CreateAt />
+        </div>
       </div>
     </div>
   );
