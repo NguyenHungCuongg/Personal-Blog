129b73dfe994242442d288543f0da201118b6f85
HungCuong <23520201@gm.uit.edu.vn>
2024-12-04 00:08:32 +0700

    Thêm component BlogContent và chỉnh lại layout

--- frontend/src/pages/ViewBlog.jsx
+++ frontend/src/pages/ViewBlog.jsx
@@ -1,12 +1,22 @@
 import BlogHeader from "../components/BlogHeader";
 import BannerImage from "../components/BannerImage";
+import BlogContent from "../components/BlogContent";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ViewBlog() {
   return (
-    <div className="container d-flex flex-column gap-4">
+    <div
+      className="container d-flex flex-column gap-4"
+      style={{
+        paddingLeft: "3rem",
+        paddingRight: "3rem",
+        marginTop: "3rem",
+        marginBottom: "3rem",
+      }}
+    >
       <BannerImage />
       <BlogHeader />
+      <BlogContent />
     </div>
   );
 }
