08105be07d83f2ec468b06c307191b1037b0d618
HungCuong <23520201@gm.uit.edu.vn>
2024-12-06 10:44:16 +0700

    Thêm các giá trị cho Tags trong Database

--- database/blog.sql
+++ database/blog.sql
@@ -1,3 +1,4 @@
+-----------------Tạo bảng-----------------
 CREATE TABLE Users (
     UserID SERIAL PRIMARY KEY,
     Username VARCHAR(50) NOT NULL UNIQUE,
@@ -27,3 +28,16 @@ CREATE TABLE TagsOfPost (
     TagID INT REFERENCES Tags(TagID),
     PRIMARY KEY (PostID, TagID)
 );
+
+-----------------Thêm giá trị cho Tags-----------------
+
+INSERT INTO Tags(TagName)
+VALUES 
+	('Art'),
+	('Technology'),
+	('Programing&Coding'),
+	('Design'),
+	('Book'),
+	('Anime&Manga'),
+	('SelfDevelopment'),
+	('Travel')
