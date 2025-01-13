f326796b247a3ed69dd8b7e9df97d211245ff164
HungCuong <23520201@gm.uit.edu.vn>
2025-01-14 00:02:13 +0700

    Revert "Xóa folder database"
    This reverts commit 714c43bea8a288b1f18935a7b0a044614c1d3046.

---
+++ database/blog.sql
@@ -0,0 +1,67 @@
+-----------------Tạo bảng-----------------
+CREATE TABLE Users (
+    UserID SERIAL PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    GoogleID VARCHAR(100),
+    ProfileImageURL VARCHAR(255),
+    UserCreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Posts (
+    PostID SERIAL PRIMARY KEY,
+    AuthorID INT REFERENCES Users(UserID),
+    Title VARCHAR(200) NOT NULL,
+    Content TEXT NOT NULL,
+    BannerImageURL VARCHAR(255),
+    PostCreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    SearchVector TSVECTOR
+);
+
+CREATE TABLE Tags (
+    TagID SERIAL PRIMARY KEY,
+    TagName VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE TagsOfPost (
+    PostID INT NOT NULL REFERENCES Posts(PostID),
+    TagID INT NOT NULL REFERENCES Tags(TagID),
+    PRIMARY KEY (PostID, TagID)
+);
+
+-----------------Thêm bảng session để lưu session-----------------
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid");
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+-----------------Thêm giá trị cho Tags-----------------
+    
+INSERT INTO Tags(TagName)
+VALUES 
+	('Art'),
+	('Technology'),
+	('Programing&Coding'),
+	('Design'),
+	('Book'),
+	('Anime&Manga'),
+	('SelfDevelopment'),
+	('Travel'),
+    ('Music'),
+    ('Life'),
+    ('Entertainment'),
+    ('Education')
+
+
+-----------------Cập nhật SearchVector-----------------
+UPDATE Posts
+SET SearchVector = to_tsvector('english',Title || ' ' || Content);
+
+-----------------Tạo index cho SearchVector-----------------
+CREATE INDEX idx_search_vector ON Posts USING gin(SearchVector);
